c2fd9acf6f961bbe0d308d11e7824574d9e146aa
Jeff Cohen <cohen.jeff@gmail.com>
2015-01-13 11:06:23 -0600

    Start with a warmup exercise

---
+++ warmup.py
@@ -0,0 +1,17 @@
+import random
+
+number_in_my_head = random.randint(1, 100)
+
+print("I'm thinking of a number between 1 and 100.")
+
+guess = eval(input("Can you guess it? "))
+
+# CHALLENGE:
+#
+# If the guess matches the number_in_my_head,
+# tell the user that they won the game, and the
+# program can end.
+#
+# Otherwise, display a message telling them whether
+# the number is higher or lower, and ask them
+# to guess again.
