ada87d72b8520f4af2e37ad819c95518c56e640e
Jeff Cohen <cohen.jeff@gmail.com>
2015-01-13 14:30:24 -0600

    Ready for class

--- files.py
+++ files.py
@@ -1,21 +1,26 @@
 # Let's learn about how to read data from files on disk.
 
+# First, let's talk a bit about strings.
+
+
+
+
 # CODE-ALONG: Read the Blackhawks roster from players.txt
 
 
 
 
 
-# CHALLNEGE: Disply the number of players on the roster.
+# CHALLNEGE: Display the number of players on the roster.
 
 
 
 
-# CHALLENGE: Find the average of the numbers in numbers.txt
+# CHALLENGE: Find the average of the numbers in numbers.txt.
 
 
 
 
-# CHALLENGE: Reproduce the landmarks list from the landmarks.txt
-# file instead of harcoding them.
+# CHALLENGE: Reproduce our fancy list of landmarks using landmarks.txt
+# instead of harcoding them.
 
--- iteration.py
+++ iteration.py
@@ -14,11 +14,16 @@ print(5)
 
 
 # 2. CODE-ALONG: Use a for loop (combined with a range) to generalize our code.
+#    (also use IDLE to experiment)
+
+
+
+# 3. CODE-ALONG: Use a for loop (combined with a list) to generalize our code.
 
 
 
 
-# 3. CHALLENGE: Choose a list structure and a looping construct to automate this code as much as you can:
+# 4. CHALLENGE: Choose a list structure and a looping construct to automate this code as much as you can:
 
 # print("Wrigley Field")
 # print("The Bean")
@@ -27,9 +32,9 @@ print(5)
 # print("The Shakespeare Theater")
 
 
-# 4. CHALLENGE: Write a function that accepts a list of landmark names you want to visit
+# 5. CHALLENGE: Write a function that accepts a list of landmark names you want to visit
 # and displays the total cost of the trip.  Use the cost information from functions.py.
 
 
-# 5. CHALLENGE: Go back to warmup.py, and enhance the code so that
+# 6. CHALLENGE: Go back to warmup.py, and enhance the code so that
 # the user can keep guessing until they get it right.
