c6d3d91bc0dbfc6340fa490a58721a739ff01a86
Jeff Cohen <cohen.jeff@gmail.com>
2015-01-13 18:35:34 -0600

    Add a display function

--- functions.py
+++ functions.py
@@ -1,15 +1,23 @@
 # Let's learn about functions, the DRY principle,
 # function arguments/parameters, and scope.
-
+def display(venue):
+  #venue
+  print(venue + " costs $40 and is located at 1033 W. Addison St.")
 
 
 print("Chicago Landmarks")
 print("-----------------")
 
-print("Wrigley Field costs $40 and is located at 1033 W. Addison St.")
-print("The Bean costs $0 and is at Millenium Park.")
-print("The John Hancock Tower costs $35 and is at 875 N. Michigan Ave.")
-print("The United Center costs $75 and is at 1901 W. Madison St.")
-print("The Shakespeare Theater costs $40 is at Navy Pier.")
+display("Wrigley Field")
+display("The Bean")
+# display()
+# display()
+# display()
+
+# print("Wrigley Field costs $40 and is located at 1033 W. Addison St.")
+# print("The Bean costs $0 and is at Millenium Park.")
+# print("The John Hancock Tower costs $35 and is at 875 N. Michigan Ave.")
+# print("The United Center costs $75 and is at 1901 W. Madison St.")
+# print("The Shakespeare Theater costs $40 is at Navy Pier.")
 
 
--- warmup.py
+++ warmup.py
@@ -6,6 +6,16 @@ print("I'm thinking of a number between 1 and 100.")
 
 guess = eval(input("Can you guess it? "))
 
+if guess == number_in_my_head:
+  print("Whoa that was insane!")
+
+if guess < number_in_my_head:
+  print("Nice, but too low.")
+
+if guess > number_in_my_head:
+  print("Nice, but too high.")
+
+
 # CHALLENGE:
 #
 # The user gets one guess.
